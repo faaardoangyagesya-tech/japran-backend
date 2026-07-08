40689042dfb960dbcfab111a83163561e4e816d7
Japran Deploy <deploy@japran.com>
2026-07-08 14:20:56 +0700

    Use /health endpoint separate from Laravel for healthcheck

--- public/router.php
+++ public/router.php
@@ -1,12 +1,13 @@
 <?php
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+if ($uri === '/health') {
+    echo 'OK';
+    return true;
+}
+
 if ($uri !== '/' && file_exists(__DIR__ . $uri)) {
     return false;
 }
-try {
-    require __DIR__ . '/index.php';
-} catch (Throwable $e) {
-    http_response_code(500);
-    header('Content-Type: application/json');
-    echo json_encode(['error' => $e->getMessage(), 'file' => $e->getFile(), 'line' => $e->getLine()]);
-}
+
+require __DIR__ . '/index.php';
